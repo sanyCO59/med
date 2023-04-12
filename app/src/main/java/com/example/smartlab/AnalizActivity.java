d9a2cf40b4a88ebb2173904afc9c199a255d977e
Lazarev_A_V <email@gmail.com>
2023-04-12 11:57:24 +0300

    session 3

--- app/src/main/java/com/example/smartlab/AnalizActivity.java
+++ app/src/main/java/com/example/smartlab/AnalizActivity.java
@@ -1,30 +1,44 @@
 package com.example.smartlab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class AnalizActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analiz);
+
     }
 
     public void support(View view) {
         Intent intent = new Intent(AnalizActivity.this, SupportActivity.class);
-        startActivity(intent);
-    }
-    public void result(View view) {
-        Intent intent = new Intent(AnalizActivity.this, ResultActivity.class);
-        startActivity(intent);
-    }
-    public void profile(View view) {
-        Intent intent = new Intent(AnalizActivity.this, ProfileActivity.class);
-        startActivity(intent);
+        startActivity(intent);//Описание: переход к классу "Поддержка" дата: 12.04.2023, автор: Лазарев А.В.
     }
 
+    public void result(View view) {
+        Intent intent = new Intent(AnalizActivity.this, ResultActivity.class);
+        startActivity(intent);//Описание: переход к классу "Результаты" дата: 12.04.2023, автор: Лазарев А.В.
+    }
+
+    public void profile(View view) {
+        Intent intent = new Intent(AnalizActivity.this, ProfileActivity.class);
+        startActivity(intent);//Описание: переход к классу "Профиль" дата: 12.04.2023, автор: Лазарев А.В.
+    }
+
+    public void open(View view) {
+
+    }
 }
--- app/src/main/java/com/example/smartlab/ProfileActivity.java
+++ app/src/main/java/com/example/smartlab/ProfileActivity.java
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
-
+private ImageView imageView16;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        imageView16 = findViewById(R.id.imageView16);
     }
     public void analiz(View view) {
+
         Intent intent = new Intent(ProfileActivity.this, AnalizActivity.class);
         startActivity(intent);
+       // Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+       // startActivityForResult(intent());
     }
     public void result(View view) {
         Intent intent = new Intent(ProfileActivity.this, ResultActivity.class);
